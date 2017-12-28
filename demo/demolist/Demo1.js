f97cce6bc1f8402cd8dfd3a319d960f6b9b0aad9
wangshih@yonyou.com <wangshih@yonyou.com>
2017-12-28 15:23:24 +0800

    模版项目

---
+++ demo/demolist/Demo1.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @title 默认按钮
+ * @description 基础按钮
+ *
+ */
+
+import React, { Component } from 'react';
+import Temp from '../../src';
+
+export default class Demo1 extends Component {
+    render () {
+        return (
+            <div className="demoPadding">
+                <Temp></Temp>
+            </div>
+        )
+    }
+}
---
+++ src/temp/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import "./style.css"
+ 
+const propTypes = {
+}
+
+class Temp extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() { 
+    return (
+      <div className="temp-main">
+        helow Temp
+      </div>
+    );
+  }
+}
+
+export default Temp;
