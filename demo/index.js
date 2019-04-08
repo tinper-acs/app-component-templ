fd08874487c544442f4e3ffa45e571e933772905
liushaozhen <804199994@qq.com>
2019-04-08 10:28:39 +0800

    展示代码风格修改

--- demo/index.js
+++ demo/index.js
@@ -1,25 +1,85 @@
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader' // eslint-disable-line
+import { Con, Row, Col } from 'bee-layout';
+import { Panel } from 'bee-panel';
+import Drawer from 'bee-drawer';
+import Clipboard from 'bee-clipboard'; 
+import './demo.scss'
 
-import DemoGroup from './index.jsx';
 
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.querySelector("#root")
-  )
+import Demo1 from "./demolist/Demo1";
+var DemoArray = [{"example":<Demo1 />,"title":" 应用组件名称","code":"/**\n *\n * @title 应用组件名称\n * @description 应用组件描述\n *\n */\n\nimport React, { Component } from 'react';\nimport Temp from '../../src/index';\nclass Demo1 extends Component {\n    render () {\n        return (\n            <div className=\"demoPadding\">\n                <Temp >tools测试代码</Temp>\n            </div>\n        )\n    }\n}\n\n\n","desc":" 应用组件描述"}]
+
+
+class Demo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    handleClick=()=> {
+        this.setState({ open: !this.state.open })
+    }
+    fCloseDrawer=()=>{
+        this.setState({
+            open: false
+        })
+    }
+
+    render () {
+        const { title, example, code, desc, scss_code  } = this.props;
+
+        const header = (
+            <div>
+                <p className='component-title'>{ title }</p>
+                <p>{ desc }</p>
+                <span className='component-code' onClick={this.handleClick}> 查看源码 <i className='uf uf-arrow-right'/> </span>
+            </div>
+        );
+        return (
+            <Col md={12} id={title.trim()} className='component-demo'>
+            <Panel header={header}>
+                {example}
+            </Panel>
+           
+            <Drawer className='component-drawerc' title={title} show={this.state.open} placement='right' onClose={this.fCloseDrawer}>
+            <div className='component-code-copy'> JS代码 
+                <Clipboard action="copy" text={code}/>
+            </div>
+            <pre className="pre-js">
+                <code className="hljs javascript">{ code }</code>
+            </pre >
+            {!!scss_code ?<div className='component-code-copy copy-css'> SCSS代码 
+                <Clipboard action="copy" text={scss_code}/>
+            </div>:null }
+                { !!scss_code ? <pre className="pre-css">
+                 <code className="hljs css">{ scss_code }</code>
+                 </pre> : null }
+            </Drawer>
+        </Col>
+    )
+    }
 }
 
-render(DemoGroup)
+class DemoGroup extends Component {
+    constructor(props){
+        super(props)
+    }
+    render () {
+        return (
+            <Row>
+            {DemoArray.map((child,index) => {
 
-if (module.hot) {
-  module.hot.accept('./index.jsx', () => {
-    const NextApp = require('./index.jsx').default // eslint-disable-line
-    render(NextApp)
-  })
+                return (
+            <Demo example= {child.example} title= {child.title} code= {child.code} scss_code= {child.scss_code} desc= {child.desc} key= {index}/>
+    )
+
+    })}
+    </Row>
+    )
+    }
 }
+
+ReactDOM.render(<DemoGroup/>, document.getElementById('root'));
